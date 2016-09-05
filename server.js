500ba1ef74ac5505e7b392bf9e79e68222c03eb1
Andy Justice <andyj@materialflow.com>
2016-09-04 23:17:21 -0700

    Reworked Folder Structure
    Node now serving files correctly.
    
    Next Step API

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 var express = require('express');
 var app = express();
+var path = require('path');
+
+/* Static Assets */
 app.use(express.static(path.join(__dirname, 'elements')));
+app.use(express.static(path.join(__dirname, 'bower_components')));
+app.use(express.static(path.join(__dirname, 'media')));
+
+
 app.get('/', function (req, res) {
-  res.sendfile('index.html');
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(3000, function () {
